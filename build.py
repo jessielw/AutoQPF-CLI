4104a2c9da886a8da33c71770ff1ccc7f1ce46a1
jlw4049 <jlw_4049@hotmail.com>
2023-07-23 01:43:34 -0400

    - Add automated build script

---
+++ build.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from subprocess import run
+import os
+
+
+def build_app():
+    # define and create pyinstaller output path
+    pyinstaller_folder = Path(Path(__file__).parent / "pyinstaller_build")
+    pyinstaller_folder.mkdir(exist_ok=True)
+
+    # define paths before changing directory
+    script = Path(Path.cwd() / "autoqpf-cli.py")
+    icon_path = Path(Path.cwd() / "autoqpf.ico")
+
+    # change directory so we output all of pyinstallers files in it's own folder
+    os.chdir(pyinstaller_folder)
+
+    # run pyinstaller command
+    build_job = run(
+        [
+            "pyinstaller.exe",
+            "-n",
+            "autoqpf-cli",
+            "--onefile",
+            f"--icon={str(icon_path)}",
+            str(script),
+        ]
+    )
+
+    # ensure output of exe
+    success = "Did not complete successfully"
+    if (
+        Path(Path("dist") / "autoqpf-cli.exe").is_file()
+        and str(build_job.returncode) == "0"
+    ):
+        success = f'\nSuccess!\nPath to exe: {str(Path.cwd() / (Path(Path("dist") / "autoqpf-cli.exe")))}'
+
+    # change directory back to original directory
+    os.chdir(script.parent)
+
+    # return success message
+    return success
+
+
+if __name__ == "__main__":
+    build = build_app()
+    print(build)
